03da3cdbfc8c8cf235e79edc04d53e0535253540
tudorgabriel24 <tudorgabriel1398@gmail.com>
2020-05-30 15:26:46 +0300

    start build router

--- js/login.js
+++ js/login.js
@@ -1,10 +1,7 @@
-
-var loginSwitch = document.querySelector(".switch-login");
-var registerSwitch = document.querySelector(".switch-register");
-var confrimationInput = document.querySelectorAll(".confirm");
-var activeForm = "login";
-
-loginSwitch.addEventListener('click', (event) => {
+function onClickLogin(event) {
+    let loginSwitch = document.querySelector(".switch-login");
+    let registerSwitch = document.querySelector(".switch-register");
+    let confrimationInput = document.querySelectorAll(".confirm");
     event.preventDefault();
     if(activeForm === "register") {
         loginSwitch.classList.add('active-form');
@@ -13,12 +10,13 @@ loginSwitch.addEventListener('click', (event) => {
             confrimationInput[index].classList.add('hide');
         }
         activeForm = "login";
-        // alert(`${loginSwitch.classList} ${registerSwitch.classList} `);
     } 
+}
 
-});
-
-registerSwitch.addEventListener('click', (event) => {
+function onClickRegister(event) {
+    let loginSwitch = document.querySelector(".switch-login");
+    let registerSwitch = document.querySelector(".switch-register");
+    let confrimationInput = document.querySelectorAll(".confirm");
     event.preventDefault();
     if(activeForm === "login") {
         registerSwitch.classList.add('active-form');
@@ -27,7 +25,5 @@ registerSwitch.addEventListener('click', (event) => {
         for (var index=0; index < confrimationInput.length; index++) {
             confrimationInput[index].classList.remove('hide');
         }
-        // confrimationInput.setAttribute("style","display:initial");
-        // alert(`${loginSwitch.classList} ${registerSwitch.classList} `);
     }
-});
+}
