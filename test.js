298b0f6049d7802d2f55ca6c3bbd82ef2ed12a6f
Dragos <38375037+dragos134@users.noreply.github.com>
2020-06-05 17:53:03 +0300

    starting with the database

---
+++ test.js
@@ -0,0 +1,59 @@
+const mysql = require("mysql");
+const crypto = require("crypto");
+
+function query(sql) {
+    var conn = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "colr",
+    });
+    conn.connect(function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log("Connected!");
+        conn.query(sql, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log("Result: " + result);
+        });
+    });
+}
+
+function insert(table, jsonData) {
+    var colums = [];
+    var values = [];
+    var hasID = false;
+    const hash = crypto.createHash("md5");
+    for (var key in jsonData) {
+        colums.push("'" + key + "'");
+        if (key == "ID") {
+            hash.update(Date.now().toString());
+            values.push("'" + hash.digest('hex') + "'");
+            hasID = true;
+        }
+        else {
+            values.push("'" + jsonData[key] + "'");
+        }
+    }
+    if (hasID == false) {
+        colums.push("'ID'");
+        hash.update(Date.now().toString());
+        values.push("'" + hash.digest('hex') + "'");
+    }
+
+    hash.end();
+
+    colums = colums.join();
+    values = values.join();
+    console.log(colums, values);
+    
+    var sql = `INSERT INTO ${table} (${colums}) VALUES (${values})`;
+    console.log(sql);
+    // query(sql);
+    
+}
+
+insert('users', {'brand': 'cuca cola', 'year': 1998});
