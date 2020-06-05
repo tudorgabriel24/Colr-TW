d47ddcec6f298b91f571031f978daed0ef781b29
Dragos <38375037+dragos134@users.noreply.github.com>
2020-06-05 18:47:31 +0300

    functiile de insert pt useri si articole

--- test.js
+++ test.js
@@ -9,18 +9,24 @@ var conn = mysql.createConnection({
 });
 
 function query(sql) {
-    conn.connect(function(err) {
+    conn.query(sql, function(err, results, fields) {
         if (err) {
             throw err;
         }
-        console.log("Connected!");
-        conn.query(sql, function(err, result) {
-            if (err) {
-                throw err;
-            }
-            console.log("Result: " + result);
-        });
+        console.log(results);
     });
+    // conn.connect(function(err) {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     console.log("Connected!");
+    //     conn.query(sql, function(err, result) {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         console.log("Result: " + result);
+    //     });
+    // });
 }
 
 function insert(table, jsonData) {
@@ -54,7 +60,40 @@ function insert(table, jsonData) {
     var sql = `INSERT INTO ${table} (${colums}) VALUES (${values})`;
     console.log(sql);
     query(sql);
-    
 }
 
-insert('articles', {'brand': 'cuca cola', 'year': 1998});
+function insertArticle(userId, name, year, brand, alcoholic, country, description, currentState) {
+    const articleHash = crypto.createHash("md5");
+    const hash = crypto.createHash("md5");
+    articleHash.update(Date.now().toString());
+    var articleID = articleHash.digest('hex');
+    var articleSQL = `INSERT INTO articles(ID, name, year, brand, alcoholic, country, description, currentState) VALUES ('${articleID}', '${name}', ${year}, '${brand}', ${alcoholic}, '${country}', '${description}', '${currentState}')`;
+    console.log(articleSQL);
+    query(articleSQL);
+    articleHash.end();
+    hash.update(Date.now().toString());
+    var bondSQL = `INSERT INTO users_articles(ID, id_user, id_article) VALUES('${hash.digest('hex')}', '${userId}', '${articleID}');`;
+    console.log(bondSQL);
+    query(bondSQL);
+    hash.end();
+
+}
+
+function insertUser(admin, fullName, email, password) {
+    const idHash = crypto.createHash("md5");
+    idHash.update(Date.now().toString());
+    const passHash = crypto.createHash("sha256");
+    passHash.update(password);
+    var userSQL = `INSERT INTO users(ID, admin, fullName, email, password) VALUES('${idHash.digest('hex')}', ${admin}, '${fullName}', '${email}', '${passHash.digest('hex')}')`;
+    console.log(userSQL);
+    query(userSQL);
+    idHash.end();
+    passHash.end();
+}
+
+// insert('articles', {'brand': 'cuca cola', 'year': 1998});
+
+// insertUser(0, 'Ciocan Dragos', 'dragos@ciocan.com', '1234');
+
+insertArticle('67ad7c87fde38897fa717203061a6149', 'capac', 2000, 'pepsy', 0, 'Romania', 'un capac din 2000', 'ruginit');
+
