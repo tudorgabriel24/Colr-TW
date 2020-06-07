a8487a5c286000c5a1045ad4b8942c00fd1b63ad
Dragos <38375037+dragos134@users.noreply.github.com>
2020-06-07 15:15:54 +0300

    added update delete

--- test.js
+++ test.js
@@ -14,19 +14,9 @@ function query(sql) {
             throw err;
         }
         console.log(results);
+        // return results;
     });
-    // conn.connect(function(err) {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     console.log("Connected!");
-    //     conn.query(sql, function(err, result) {
-    //         if (err) {
-    //             throw err;
-    //         }
-    //         console.log("Result: " + result);
-    //     });
-    // });
+    return 2;
 }
 
 function insert(table, jsonData) {
@@ -62,21 +52,15 @@ function insert(table, jsonData) {
     query(sql);
 }
 
+
+
 function insertArticle(userId, name, year, brand, alcoholic, country, description, currentState) {
     const articleHash = crypto.createHash("md5");
     const hash = crypto.createHash("md5");
     articleHash.update(Date.now().toString());
     var articleID = articleHash.digest('hex');
-    var articleSQL = `INSERT INTO articles(ID, name, year, brand, alcoholic, country, description, currentState) VALUES ('${articleID}', '${name}', ${year}, '${brand}', ${alcoholic}, '${country}', '${description}', '${currentState}')`;
-    console.log(articleSQL);
+    var articleSQL = `INSERT INTO articles(ID, user_id, name, year, brand, alcoholic, country, description, currentState) VALUES ('${articleID}', '${userId}', '${name}', ${year}, '${brand}', ${alcoholic}, '${country}', '${description}', '${currentState}')`;
     query(articleSQL);
-    articleHash.end();
-    hash.update(Date.now().toString());
-    var bondSQL = `INSERT INTO users_articles(ID, id_user, id_article) VALUES('${hash.digest('hex')}', '${userId}', '${articleID}');`;
-    console.log(bondSQL);
-    query(bondSQL);
-    hash.end();
-
 }
 
 function insertUser(admin, fullName, email, password) {
@@ -91,9 +75,91 @@ function insertUser(admin, fullName, email, password) {
     passHash.end();
 }
 
+function updateUser(userId, fullName, email, password) {
+    var update = [];
+    if (fullName != null) {
+        update.push(`fullName = '${fullName}'`);
+    }
+    if (email != null) {
+        update.push(`email = '${email}`);
+    }
+    if (password != null) {
+        const hash = crypto.createHash("sha256");
+        hash.update(password);
+        update.push(`password = '${hash.digest('hex')}`);
+    }
+
+    if (update.length == 0) {
+        return;
+    }
+
+    update = update.join();
+
+    var updateSQL = `UPDATE users SET ${update} WHERE ID = '${userId}'`;
+
+    console.log(updateSQL);
+
+    query(updateSQL);
+}
+
+function updateArticle(articleId, name, year, brand, alcoholic, country, description, currentState) {
+    var update = [];
+    if (name != null) {
+        update.push(`name = '${name}'`);
+    }
+    if (year != null) {
+        update.push(`year = '${year}'`);
+    }
+    if (brand != null) {
+        update.push(`brand = '${brand}'`);
+    }
+    if (alcoholic != null) {
+        update.push(`alcoholic = '${alcoholic}'`);
+    }
+    if (country != null) {
+        update.push(`country = '${country}'`);
+    }
+    if (description != null) {
+        update.push(`description = '${description}'`);
+    }
+    if (currentState != null) {
+        update.push(`currentState = '${currentState}'`);
+    }
+
+    if (update.length == 0) {
+        return;
+    }
+
+    update = update.join();
+
+    var updateSQL = `UPDATE articles SET ${update} WHERE ID = '${articleId}'`;
+
+    query(updateSQL);
+}
+
+
+
+function deleteUser(userId) {
+    query(`DELETE FROM users WHERE ID = '${userId}'`);
+}
+
+function deleteArticle(articleId) {
+    query(`DELETE FROM articles WHERE ID = '${articleId}'`);
+}
+
+function deleteEntry(table, id) {
+    query(`DELETE FROM '${table}' WHERE ID = '${id}'`);
+}
+
 // insert('articles', {'brand': 'cuca cola', 'year': 1998});
 
-// insertUser(0, 'Ciocan Dragos', 'dragos@ciocan.com', '1234');
+// insertUser(0, 'asd', 'marina@marian.com', '12345');
 
-insertArticle('67ad7c87fde38897fa717203061a6149', 'capac', 2000, 'pepsy', 0, 'Romania', 'un capac din 2000', 'ruginit');
+insertArticle('b8ad2480f1870d522629cabd378ae122', 'mare capac', 2002, 'timisoreana', 0, 'Romania', 'bere pet', 'sters');
+
+// updateUser('67ad7c87fde38897fa717203061a6149', null, null, null);
+
+// console.log(query(`SELECT * FROM users`));
+
+// deleteUser('67ad7c87fde38897fa717203061a6149');
 
