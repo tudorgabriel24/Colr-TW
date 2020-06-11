dc8dd2d394b1a8b6eedf27b299ef9e3de332b5ab
AndreiR <andreir98@yahoo.com>
2020-06-11 14:54:35 +0300

    Modified

--- index.js
+++ index.js
@@ -1,48 +1,15 @@
-// var page = "login";
-// var activeForm = "login";
-
-// window.onload = function() {
-//   console.log("DOM HAS LOADED");
-
-//   var Router = function(name, routes) {
-
-//     return {
-//       name: name,
-//       routes
-//     }
-
-//   };
-
-//   var mainRouter = new Router('mainRouter', [
-//     {
-//       path: '/login',
-//       name: 'login'
-//     },
-//     {
-//       path: '/news',
-//       name: 'news'
-//     }
-//   ]);
-//   console.log('mainRouter = ' + mainRouter);
-//   var currentPath = window.location.pathname;
-//   console.log('currentPath = '+ currentPath);
-// }
-
-// window.onpopstate = () =>  fetchLoginPage();
-
-// (async function () {
-//   await fetchLoginPage();
-// })();
-
-// async function fetchLoginPage() {
-//   await fetch('./html/login.html')
-//   .then(function(response) {
-//     return response.text();
-//   })
-//   .then(function(body) {
-//     console.log("aici");
-//     history.pushState({},'',`/login`);
-//     document.querySelector('#content').innerHTML = body;
-//     // console.log(window.location.search);
-//   });
-// }
+window.onload = function makeContentRequest() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    console.log(xhttp.responseText);
+  };
+  xhttp.open("GET", "http://localhost:3000/articles", true);
+  xhttp.getResponseHeader("Access-Control-Allow-Origin", "*");
+  xhttp.getAllResponseHeaders("Access-Control-Allow-Origin", "*");
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.setRequestHeader(
+    "Authorization",
+    this.localStorage.getItem("Authorization")
+  );
+  xhttp.send();
+};
