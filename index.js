42979276c29173f413a1bf131ee98dcc1b6cb4e8
Dragos <38375037+dragos134@users.noreply.github.com>
2020-06-12 09:24:52 +0300

    request to add in cart

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ window.onload = function makeContentRequest() {
 function navigateToLogin() {
   window.location.replace('http://localhost:5500/html/login.html');
 }
+
+var btn = document.getElementsByClassName('addbtn');
+btn.addEventListener('click', () => {
+  const xhttp = XMLHttpRequest();
+  xhttp.onload = function() {
+    console.log('yay');
+  }
+  xhttp.open('POST', 'http://localhost:3000/cart', true);
+                
+  xhttp.getResponseHeader('Access-Control-Allow-Origin', '*');
+  xhttp.getAllResponseHeaders('Access-Control-Allow-Origin', '*');
+  request.setRequestHeader('Content-Type', 'application/json');
+  // console.log(requestData.image.length);
+  xhttp.send({'id_article': btn.id});
+});
