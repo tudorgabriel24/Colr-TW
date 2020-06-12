9a3f5b9328f4bd32c20560afffc2ef99eca4c0be
AndreiR <andreir98@yahoo.com>
2020-06-12 07:31:21 +0300

    Update

--- asd.js
+++ asd.js
@@ -4,34 +4,33 @@ const crypto = require("crypto");
 // var conn = require('./project-node-app/server').connection;
 // console.log(conn);
 var conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "colr",
-    charset: "utf8_general_ci",
-  });
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "colr",
+  charset: "utf8_general_ci",
+});
 
 function query(sql) {
-    return new Promise((resolve, reject) => {
-        // console.log(conn);
-        conn.query(sql, function(err, results, fields) {
-            if (err) {
-                reject({'status': 404, 'description': err});
+  return new Promise((resolve, reject) => {
+    // console.log(conn);
+    conn.query(sql, function (err, results, fields) {
+      if (err) {
+        reject({ status: 404, description: err });
 
-                // throw err;
-            }
-            resolve(results);
-        });
+        // throw err;
+      }
+      resolve(results);
     });
   });
 }
 
 async function getArticles() {
-    return await query(`SELECT * FROM articles ORDER BY views`);
+  return await query(`SELECT * FROM articles ORDER BY views`);
 }
 
 async function getCart(articles) {
-    return await query(`SELECT * FROM articles WHERE ID in (${articles})`);
+  return await query(`SELECT * FROM articles WHERE ID in (${articles})`);
 }
 
 exports.getCart = getCart;
